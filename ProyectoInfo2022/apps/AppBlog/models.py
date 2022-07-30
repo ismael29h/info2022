81de00a4edc8ec6f85fcb6eea8f3030048f06a60
MatiasEzequiel12 <matias_palaciio@hotmail.com>
2022-07-30 17:24:08 -0300

    1. instalacion de requerimientos, runtime(opcional por version) 2. agregando modelo.py(contenido de portada de blog) 3. agregando cosas ( base(que seria setting), admin, 4. creacion del super user (user:grupo6 - pass: grupseis)

--- ProyectoInfo2022/apps/AppBlog/models.py
+++ ProyectoInfo2022/apps/AppBlog/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 
 #....
-class Blog(models.Model):
-    pass
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-date_added']
 
--- ProyectoInfo2022/settings/base.py
+++ ProyectoInfo2022/settings/base.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProyectoInfo2022.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(BASE_DIR), 'ProyectoInfo2022\templates')],
+        'DIRS': [os.path.join(os.path.dirname(BASE_DIR), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,11 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), 'ProyectoInfo2022\static'),
-)
+STATICFILES_DIRS = [ os.path.join(os.path.dirname(BASE_DIR), 'ProyectoInfo2022\static'),
+]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'ProyectoInfo2022\media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 
 
